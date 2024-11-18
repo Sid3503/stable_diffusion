2d9069b7473daae44c978acf0cc87241e85b648b
Sid3503 <mishrasiddharh072@gmail.com>
2024-11-18 18:13:01 +0530

    decoder complete

--- sd/decoder.py
+++ sd/decoder.py
@@ -66,3 +66,64 @@ class VAE_ResidualBlock(nn.Module):
         x = self.conv_2(x)
 
         return x + self.residual_layer(residue)
+    
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock(512),
+
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch, 512, H/8, W/8) -> (Batch, 512, H/8, W/8)
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch, 512, H/8, W/8) -> (Batch, 512, H/4, W/4)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch, 512, H/4, W/4) -> (Batch, 512, H/2, W/2)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+            
+            # (Batch, 512, H/2, W/2) -> (Batch, 256, H, W)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+
+            nn.GroupNorm(32, 128),
+
+            nn.SiLU(),
+
+            # (Batch, 128, H, W) -> (Batch, 3, H, W)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch, 4, H/8, W/8)
+        x /= 0.18215
+
+        for module in self:
+            x = module(x)
+
+        # (Batch, 3, H, W)
+        return x
