30f8a2d66f1a05b0a08bb42f7c6655d66dbf4e0b
Sid3503 <mishrasiddharh072@gmail.com>
2025-01-01 23:18:03 +0530

    refactor

--- sd/model_loader.py
+++ sd/model_loader.py
@@ -9,16 +9,16 @@ def preload_models_from_standard_weights(ckpt_path, device):
     state_dict = model_converter.load_from_standard_weights(ckpt_path, device)
 
     encoder = VAE_Encoder().to(device)
-    encoder.load_state_dict(state_dict["encoder"], strict=True)
+    encoder.load_state_dict(state_dict["encoder"], strict=False)
 
     decoder = VAE_Decoder().to(device)
-    decoder.load_state_dict(state_dict["decoder"], strict=True)
+    decoder.load_state_dict(state_dict["decoder"], strict=False)
 
     diffusion = Diffusion().to(device)
-    diffusion.load_state_dict(state_dict["diffusion"], strict=True)
+    diffusion.load_state_dict(state_dict["diffusion"], strict=False)
 
     clip = CLIP().to(device)
-    clip.load_state_dict(state_dict["clip"], strict=True)
+    clip.load_state_dict(state_dict["clip"], strict=False)
 
 
     return {
